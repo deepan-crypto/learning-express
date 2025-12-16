faaeb3c79ded68f90cb287af5c000ee52e9d34b0
deepan-crypto <deepang467@gmail.com>
2025-12-16 11:23:17 +0530

    server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
-const app = require("./index");
+const app = require("./index.js");
 
-console.log(process.env.PORT_NO);
+const PORT = process.env.PORT_NO || 3000;
 
-app.listen(process.env.PORT_NO, () => {
-    console.log("Server started successfully on port", process.env.PORT_NO);
+app.listen(PORT, () => {
+    console.log(`Server started successfully on port ${PORT}`);
 });
