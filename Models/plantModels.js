eee9ffb05eed400cfd0dc5f19012894c51b6dd12
deepan-crypto <deepang467@gmail.com>
2025-12-16 12:40:35 +0530

    schema

--- Models/plantModels.js
+++ Models/plantModels.js
@@ -1,6 +1,30 @@
 const mongoose = require("mongoose");
 
 const plantSchema = new mongoose.Schema({
-
-    
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  waterFrequency: {
+    type: String,
+    required: true
+  },
+  sunlight: {
+    type: String,
+    required: true
+  }
 });
+
+const Plant = mongoose.model("Plant", plantSchema);
+
+module.exports = Plant;
