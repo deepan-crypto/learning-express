9ab44070fe54d1bb69338108f9ea75ad955fcc75
deepan-crypto <deepang467@gmail.com>
2025-12-17 09:24:58 +0530

    authRoutes

--- Router/authRoutes.js
+++ Router/authRoutes.js
@@ -1,9 +1,12 @@
 const express=require("express");
+const authController=require("../controllers/authController");
+
 
 const authRoutes=express.Router();
-authRoutes.route("/login").post();
-authRoutes.route("/signup").post();
-authRoutes.route("/getAllUsers").get();
+
+authRoutes.route("/login").post(authController.login);
+authRoutes.route("/signup").post(authController.signup);
+authRoutes.route("/getAllUsers").get(authController.allUsers);
 
 module.exports=authRoutes;
 
